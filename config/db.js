30786827d80501ecf638e7a8852fb0ce7888554a
grewalarun <arunkumargrewal@gmail.com>
2026-02-10 12:07:48 +0530

    vercel admin

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const MONGO_URI = "mongodb+srv://Vercel-Admin-task-management:4qpJb4T4WSKzG0p2@task-management.sy9u0nn.mongodb.net/?retryWrites=true&w=majority";
 //const  MONGO_URI = "mongodb://127.0.0.1:27017/task_manager"
 const connectDB = async () => {
-  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+  console.log("MONGO_URI exists:", !!process.env.MONGODB_URI);
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("MongoDB connected successfully");
   } catch (error) {
