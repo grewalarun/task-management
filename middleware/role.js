27cbbe932ec74ea0b76b504b960b17b4700a83f5
grewalarun <arunkumargrewal@gmail.com>
2026-05-02 14:40:49 +0530

    error message updated

--- middleware/role.js
+++ middleware/role.js
@@ -1,7 +1,7 @@
 const role = (requiredRole) => {
   return (req, res, next) => {
     if (req.user.role !== requiredRole) {
-      return res.status(403).json({ message: "Access denied" });
+      return res.status(403).json({ message: `Access denied. Only ${requiredRole} can perform this action.` });
     }
     next();
   };
